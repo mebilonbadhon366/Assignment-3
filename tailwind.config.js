2b916b3bc366c98f5533d823eabc5e5ee1a56cb3
mebilonbadhon366 <mebilonhossainbd@gmail.com>
2024-08-27 13:19:59 +0600

    added a section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,13 @@ module.exports = {
     },
     plugins: [],
   }
+  module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/assets/bg.jpg')",
+          'footer-texture': "url('/img/footer-texture.png')",
+        }
+      }
+    }
+  }
